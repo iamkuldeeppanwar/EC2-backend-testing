7bb4ab0a397a055077b993752a3e2fbdff57b944
iamkuldeeppanwar <kuldeep10panwar@gmail.com>
2024-10-17 14:18:41 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 6000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    name: process.env.USER,
+    message: "Hosted Successfully!",
+    data: "Secure ho gaya hai kya?",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
