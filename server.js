7eaa9b1f3a00979af92f3cea2cff5b486a110c19
iamkuldeeppanwar <kuldeep10panwar@gmail.com>
2024-10-20 15:24:26 +0530

    Testing again

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ const port = process.env.PORT || 6000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    name: process.env.USER,
-    message: "Hosted Successfully!",
-    data: "Secure ho gaya hai !!! hurray",
-  });
+app.get("/api", (req, res) => {
+  try {
+    res.status(200).json({
+      name: process.env.USER,
+      message: "Hosted Successfully!",
+      data: "checked !!! hurray",
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
